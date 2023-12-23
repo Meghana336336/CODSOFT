b2b4b7af1428d67d1c876fb0c7d46f66a786fb7b
Meghana336336 <321506402211@andhrauniversity.edu.in>
2023-12-23 20:51:40 +0530

    first commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFF5F52EE);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
+const Color tdBgColor = Color(0xFFEEEFF5);
+
+const Color tdGreen=Color(0xFFC8E6C9);
+const Color tdGold=Color(0xFFFFF59D);
+
+const Color tdLBlueB=Color(0xFF90CAF9);
+const Color tdLBlue=Color(0xFFBBDEFB);
+
+
